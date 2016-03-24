1112dd9a3df9788fe2c72cf65250b828a178edca
Roy Wei <roywjy@gmail.com>
2016-03-24 20:50:34 +0800

    make CRUD function for controller and new and _form files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,41 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show. :edit, :update, :destroy]
 
   def index
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :company, :url)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
